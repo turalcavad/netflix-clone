20e9d3fe21c8f979b6f7f1bbe558e0ddccdf29bd
turalcavadn <turaltj@code.edu.az>
2022-06-20 03:47:10 +0400

    footer done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import react from "react";
 import { JumbotronContainer } from "./containers/jumbotron";
+import { FooterContainer } from "./containers/footer";
 
 export default function App() {
-	return <JumbotronContainer />;
+	return (
+		<>
+			<JumbotronContainer />
+			<FooterContainer />
+		</>
+	);
 }
